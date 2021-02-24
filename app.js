e4197fea5adf7346520ffa28f64c7eabd118419d
khairislama <khairi.slama@polytechnicien.tn>
2021-02-24 19:10:59 +0100

    added user authentifications

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const   express                     = require("express"),
         flash                       = require("connect-flash"),
         methodOverride              = require("method-override"),
         expressSanitizer            = require("express-sanitizer"),
+        passport                    = require("passport"),
+        localStrategy               = require("passport-local"),
+        passportLocalMongoose       = require("passport-local-mongoose"),
         app                         = express();
 
 // MODELS IMPORTATION
+const   User                        = require("./models/user");
 
 // ROUTES IMPORTATION
 const   indexRoutes                 = require("./routes/index");
@@ -21,13 +25,28 @@ app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 app.use(express.static(__dirname + "/public"));
 app.use(flash());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // DATABASE CONNEXION
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 // PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: process.env.SECRET_MESSAGE,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // GENERAL MIDDLEWARES
+app.use((req, res, next)=>{
+    res.locals.currentUser  = req.user;
+    next();
+});
 
 // CALLING ROUTES
 app.use(indexRoutes);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,53 @@
 // PACKAGE IMPORTATION
 const   express             = require("express"),
-        router              = express.Router();
+        router              = express.Router(),
+        passport            = require("passport"),
+        User                = require("../models/user");
 
 // root route
 router.get("/", (req, res)=>{
-    res.render("home");
+    res.render("landing");
 });
+
+// ***** AUTH ROUTES *****
+// show register form
+router.get("/register", (req, res)=>{
+    res.render("register");
+});
+
+//handling sign up logic
+router.post("/register", (req, res)=>{
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, (err, user)=>{
+        if(err){
+            //req.flash("error", err.message);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, ()=>{
+            //req.flash("success", `Welcome to Tunisia-camping ${user.email}`);
+            res.redirect("/");
+        });
+    });
+});
+
+//show the login form
+router.get("/login", (req,res)=>{
+    res.render("login");
+});
+
+//handling login logic
+router.post("/login",passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+}));
+
+//logout logic
+router.get("/logout", (req, res)=>{
+    req.logout();
+    //req.flash("success", "logged you out!");
+    res.redirect("/campgrounds");
+});
+
 
 // MODULE EXPORTATION
 module.exports = router;
